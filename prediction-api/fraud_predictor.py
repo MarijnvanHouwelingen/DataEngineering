872d08acb316541938c32f455017f415a706e056
Kyra Jongman <48756773+Cayah99@users.noreply.github.com>
2023-10-20 22:47:01 +0200

    Update fraud_predictor.py

--- prediction-api/fraud_predictor.py
+++ prediction-api/fraud_predictor.py
@@ -7,28 +7,33 @@ import pickle
 import logging
 from io import StringIO
 
-
 class FraudPredictor:
     def __init__(self):
         self.model = None
 
-    def predict_single_record(self, prediction_input):
-        logging.debug(prediction_input)
-        if self.model is None:
-            try:
-                model_repo = os.environ['MODEL_REPO']
-                file_path = os.path.join(model_repo, "model_assignment1.pkl")
-                with open(file_path, 'rb') as model_file:
-                    self.model = pickle.load(model_file)            
-            except KeyError:
-                print("MODEL_REPO is undefined")
-                with open('model_assignment1.pkl', 'rb') as model_file:
-                    self.model = pickle.load(model_file) 
+    # download the model
+    def download_model(self):
+        project_id = os.environ.get('PROJECT_ID')
+        model_repo = os.environ.get('MODEL_REPO')
+        model_name = os.environ.get('MODEL_NAME')
+        client = storage.Client(project=project_id)
+        bucket = client.bucket(model_repo)
+        blob = bucket.blob(model_name)
+        blob.download_to_filename('local_model.pkl')
+        with open('local_model.pkl', 'rb') as model_file:
+            self.model = pickle.load(model_file) 
+        return jsonify({'message': " the model was downloaded"}), 200
 
-        df = pd.read_json(StringIO(json.dumps(prediction_input)), orient='records')
+    def predict_single_record(self, prediction_input):
+        print(prediction_input)
+        if self.model is None:
+            self.download_model()
+        print(json.dumps(prediction_input))
+        df = pd.read_json(json.dumps(prediction_input), orient='records')
+        print(df)
         y_pred = self.model.predict(df)
-        logging.info(y_pred[0])
+        print(y_pred[0])
         status = (y_pred[0] > 0.5)
-        logging.info(type(status[0]))
+        print(type(status[0]))
         # return the prediction outcome as a json message. 200 is HTTP status code 200, indicating successful completion
         return jsonify({'result': str(status[0])}), 200
