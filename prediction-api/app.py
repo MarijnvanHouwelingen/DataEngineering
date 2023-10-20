71f85bbffc549abdc78b4c8e6b909afb67ddfe4e
Kyra Jongman <48756773+Cayah99@users.noreply.github.com>
2023-10-20 22:41:34 +0200

    Update app.py

--- prediction-api/app.py
+++ prediction-api/app.py
@@ -7,16 +7,15 @@ from fraud_predictor import FraudPredictor
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+@app.route('/fraud_predictor/model', methods=['PUT'])  # trigger updating the model
+def refresh_model():
+    return fp.download_model()
 
-@app.route('/fraud_predictor/', methods=['POST']) # path of the endpoint. Except only HTTP POST request
+@app.route('/fraud_predictor/', methods=['POST'])  # path of the endpoint. Except only HTTP POST request
 def predict_str():
     # the prediction input data in the message body as a JSON payload
     prediction_inout = request.get_json()
-    return dp.predict_single_record(prediction_inout)
+    return fp.predict_single_record(prediction_inout)
 
-
-dp = FraudPredictor()
-# The code within this conditional block will only run the python file is executed as a
-# script. See https://realpython.com/if-name-main-python/
-if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)
+fp = FraudPredictor()
+app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)
